b18c8db66ecb1c725be21a55d28ca69788b8f605
alexreborn-5310 <164221777@qq.com>
2026-06-11 17:17:45 +0800

    first commit

---
+++ solider.py
@@ -0,0 +1,32 @@
+from gun import gun
+class solider:
+    def __init__(self, name, gun=None):
+        self.name = name
+        self.gun = gun
+    def change_gun(self, new_gun):
+        self.gun = new_gun
+    def add_gun_bullets(self, bullets_num):
+        if self.gun is None:
+            print(f"{self.name} has no gun to fire")
+            return
+        self.gun.add_bullets(bullets_num)
+      
+    def fire(self):
+        if self.gun is None:
+            print(f"{self.name} has no gun to fire")
+            return
+        if (self.gun.shoot()):
+            print(f"{self.name} fired successfully")
+        else:
+            print(f"{self.name} failed to fire")
+if __name__ == "__main__":
+     alex = solider("Alex")
+     alex.change_gun(gun("AK47", 5))
+     alex.fire()
+     alex.fire()
+     alex.fire()
+     alex.fire()
+     alex.fire()
+     alex.fire() 
+     alex.add_gun_bullets(3)
+     alex.fire()
